24b5fc5eaa5cafa3d45b2bf2a8bf51f266a20c00
[josiah stelzl] <josiahstelzl@gmail.com>
2022-11-29 13:14:17 -0500

    fixed test bug

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,19 +1,19 @@
+// __tests__/About.test.js
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
 
-describe('About component', () => {
-    // first test
-    it('renders', () => {
-        render('<About />');
-    });
-
-    // second test
-    it('matches snapshot DOM node structure', () => {
-        const { asFragment } = render('<About />');
-        expect(asFragment()).toMatchSnapshot();
-    });
-})
-
 afterEach(cleanup);
+
+describe('About component', () => {
+  it('renders', () => {
+    render(<About />);
+  });
+  
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<About />);
+    
+    expect(asFragment()).toMatchSnapshot();
+  });
+})
